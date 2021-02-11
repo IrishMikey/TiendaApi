b78debd3c056910be8aad4c65e4e120c9a8d6741
IrishMikey <47180065+IrishMikey@users.noreply.github.com>
2021-02-11 13:19:40 +0000

    index+js

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
+import {tiendaXhr} from "./apis.js";
 var btns = document.getElementsByTagName("input");
 var dBtns = document.getElementById("dBtns");
 var img = document.getElementsByTagName("img")[0];
-console.log(btns);
 
 function hideBtns(){
 dBtns.style.display = "none";
@@ -9,5 +9,15 @@ img.style.display = "block"
 }
 
 [...btns].forEach(btn => {
-    btn.addEventListener("click", () => hideBtns());
+    btn.addEventListener("click", () =>{ 
+        hideBtns();
+        setReqType(btn.value);
+        console.log(btn.value)
+    });
 });
+
+function setReqType(type){
+    if(type == "XHR"){
+        tiendaXhr();
+    }
+}
